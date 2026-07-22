4278536296779c3fe55bed118876b261ed0d4f1e
reset314 <liuhongyi0504@163.com>
2026-07-22 19:35:20 +0800

    init program

---
+++ src/main/java/com/example/experience/ExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/experience/ExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.experience;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class ExperienceApplicationTests {
+
+	@Test
+	void applicationStartsWithoutCustomConfiguration() {
+		assertThat(true).isTrue();
+	}
+
+}
