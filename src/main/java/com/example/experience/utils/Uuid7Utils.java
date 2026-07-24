67e734f7e7a6e9910a50f34d710aa7fc6494b6d8
reset314 <liuhongyi0504@163.com>
2026-07-24 22:21:05 +0800

    完成uuid7生成

--- src/main/java/com/example/experience/utils/Uuid7Utils.java
+++ src/main/java/com/example/experience/utils/Uuid7Utils.java
@@ -1,7 +1,29 @@
 package com.example.experience.utils;
 
-import com.fasterxml.uuid.*;
+import java.util.UUID;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.NoArgGenerator;
 
 public class Uuid7Utils {
+
+    private static final NoArgGenerator TIME_BASED_GENERATOR = Generators.timeBasedEpochGenerator();
+    private static String removeHyphens(String uuid) {
+        return uuid.replace("-", "");
+    }
+    private static String addHyphens(String uuid) {
+        return uuid.replaceFirst(
+                "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)",
+                "$1-$2-$3-$4-$5");
+    }
+
+    public static String generateUuid7() {
+        UUID uuid = TIME_BASED_GENERATOR.generate();
+        return removeHyphens(uuid.toString());
+    }
+
+    public static String getUuidWithHyphens(String uuid) {
+        return addHyphens(uuid);
+    }
 
 }
