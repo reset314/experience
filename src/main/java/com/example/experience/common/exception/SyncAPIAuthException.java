2f5031e08ce8ab2e6541a47b204d228db6598ac4
Your Name <you@example.com>
2026-08-16 18:11:20 +0800

    create two sync exception

---
+++ src/main/java/com/example/experience/common/exception/SyncAPIAuthException.java
@@ -0,0 +1,7 @@
+package com.example.experience.common.exception;
+
+public class SyncAPIAuthException extends RuntimeException {
+    public SyncAPIAuthException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/experience/common/exception/SyncAPIException.java
@@ -0,0 +1,8 @@
+package com.example.experience.common.exception;
+
+public class SyncAPIException extends RuntimeException {
+    public SyncAPIException(String message){
+        super(message);
+    }
+
+}
